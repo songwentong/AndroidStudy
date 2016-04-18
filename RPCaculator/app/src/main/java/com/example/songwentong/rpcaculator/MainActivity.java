521826a2a15ab59b24dcc9a0afc143220e9b97cc
songwentong <songwentong@mobimagic.com>
2016-04-18 11:13:42 +0800

    页面搭建

--- RPCaculator/app/src/main/java/com/example/songwentong/rpcaculator/MainActivity.java
+++ RPCaculator/app/src/main/java/com/example/songwentong/rpcaculator/MainActivity.java
@@ -2,7 +2,16 @@ package com.example.songwentong.rpcaculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+/**
 
+ 人品计算器
+ 功能:给出姓名,得出人品
+ 输入:姓名,性别
+ 输出:点击计算,跳转到另外一个页面.在新页面给出一个人品得分,给出一个评价
+
+
+
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
---
+++ RPCaculator/app/src/main/java/com/example/songwentong/rpcaculator/ResultActivity.java
@@ -0,0 +1,20 @@
+package com.example.songwentong.rpcaculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //加载布局
+        setContentView(R.layout.activity_result);
+
+
+    }
+    public void click(View v){
+
+    }
+}
